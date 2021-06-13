c04aa8d90bb047f8f8f0b26cd57f56f54f120a23
JiangZhiYan <jaemusic@foxmail.com>
2021-06-13 16:57:13 +0800

    定时任务corn表达式修正:每3个月的1号00:00执行一次,下次执行时间:2021-7-1 00:00:00

--- src/main/java/com/jiangzhiyan/crm/Task/ScheduleTask.java
+++ src/main/java/com/jiangzhiyan/crm/Task/ScheduleTask.java
@@ -19,11 +19,11 @@ public class ScheduleTask {
      * 定时任务:每3个月执行一次lossCustomersTask()方法
      * 注解@Scheduled:周期注解
      * (cron = "0/5 * * * * ?"):corn表达式:每5秒
-     * (cron = "0 0 0 0 1/3 ?"):corn表达式:每3个月
+     * (cron = "0 0 0 1 1/3 ? "):corn表达式:每3个月的1号00:00:00
      * 定义方法执行周期
      */
     //@Scheduled(cron = "0/5 * * * * ?")
-    @Scheduled(cron = "0 0 0 0 1/3 ?")
+    @Scheduled(cron = "0 0 0 1 1/3 ? ")
     public void lossCustomersTask(){
         customerLossService.updateCustomerLossTable();
     }
