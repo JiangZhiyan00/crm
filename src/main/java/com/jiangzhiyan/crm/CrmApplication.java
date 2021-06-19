38716d6f6d09396d1efda0de903654b5ec55fb4c
JiangZhiYan <jaemusic@foxmail.com>
2021-06-19 19:53:02 +0800

    项目打包线上环境

--- src/main/java/com/jiangzhiyan/crm/CrmApplication.java
+++ src/main/java/com/jiangzhiyan/crm/CrmApplication.java
@@ -3,14 +3,29 @@ package com.jiangzhiyan.crm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author JiangZhiyan
+ */
 @SpringBootApplication
 @MapperScan(basePackages = "com.jiangzhiyan.crm.dao")
 @EnableScheduling
-public class CrmApplication {
+public class CrmApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
+    }
+
+    /**
+     * 设置web项目的启动入口
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CrmApplication.class);
     }
 }
